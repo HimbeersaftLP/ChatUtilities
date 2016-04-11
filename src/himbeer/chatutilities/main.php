d6c94129ec4598e6b9bf9ced9f88105566847a04
HimbeersaftLP <elias.nestl@outlook.de>
2016-04-11 16:03:35 +0200

    Update main.php

--- src/himbeer/chatutilities/main.php
+++ src/himbeer/chatutilities/main.php
@@ -4,6 +4,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\permission\Permission;
+use pocketmine\Server;
 class Main extends PluginBase implements Listener{
      
      public function onEnable(){
@@ -13,8 +15,25 @@ class Main extends PluginBase implements Listener{
      
      public function onCommand(CommandSender $sender, Command $command, $label, array $args){
           switch($command->getName()){
-               case "steak":
-                    //do stuff
+               case "nothing":
+                    break;
+               case "easybroadcast":
+                    if(count($args) < 1){
+                         $sender->sendMessage("Usage: /easybroadcast [message]");
+                    }elseif($sender->hasPermission("chatu.ebcast")){
+                         $this->getServer()->broadcastMessage(implode(" ",$args));
+                    }else{
+                         $sender->sendMessage("You don't have the permission to perform this command!");
+                    }
+                    break;
+               case "sendme":
+                    if(count($args) < 1){
+                         $sender->sendMessage("Usage: /sendme [message]");
+                    }elseif($sender->hasPermission("chatu.sendme")){
+                         $sender->sendMessage(implode(" ",$args));
+                    }else{
+                         $sender->sendMessage("You don't have the permission to perform this command!");
+                    }
           }
           return true;
      }
